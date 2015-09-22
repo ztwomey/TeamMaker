280f7807b927b13021b95babd082cee994bec521
ztwomey <zrt685@hotmail.com>
2015-09-21 21:55:33 -0400

    Set up array to list players that user enters

--- app/src/main/java/com/example/zac/teammaker/TeamMaker.java
+++ app/src/main/java/com/example/zac/teammaker/TeamMaker.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TeamMaker extends AppCompatActivity {
 
-    private EditText mNameField;
-    private Button mAddButton;
-    private Button mSubmitButton;
+    public EditText mNameField;
+    public Button mAddButton;
+    public Button mSubmitButton;
+    public ListView mNameList;
+    public ArrayList<String> mPlayerList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +29,20 @@ public class TeamMaker extends AppCompatActivity {
         mNameField = (EditText) findViewById(R.id.playerNames);
         mAddButton = (Button)findViewById(R.id.addPlayer);
         mSubmitButton = (Button)findViewById(R.id.subPlayers);
+        mNameList = (ListView) findViewById(R.id.nameList);
+
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = mNameField.getText().toString();
-                sendNames(name);
+                String getName = mNameField.getText().toString();
+                mPlayerList.add(getName);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(TeamMaker.this, android.R.layout.simple_list_item_1, mPlayerList);
+                mNameList.setAdapter(adapter);
+                mNameField.setText("");
             }
-        });
 
-    }
-    private void sendNames(String name) {
-        Intent intent = new Intent(this, Names.class);
-        intent.putExtra("name", name);
-        startActivity(intent);
+
+        });
     }
 
 }
