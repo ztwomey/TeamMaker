7621891c89d8e42b63d4292589cc6192e4490570
ztwomey <zrt685@hotmail.com>
2015-09-30 18:53:57 -0400

    Added to layout of activity_results.xml and added variables and onclicklistener to ResultsActivity.java.

--- app/src/main/java/com/example/zac/teammaker/ResultsActivity.java
+++ app/src/main/java/com/example/zac/teammaker/ResultsActivity.java
@@ -6,21 +6,42 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ResultsActivity extends AppCompatActivity {
 
     public static final String TAG = ResultsActivity.class.getSimpleName();
+    public EditText mNumOfTeams;
+    public Button mCreateTeams;
+    public ListView mTeamOne;
+    public ListView mTeamTwo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
+        mNumOfTeams = (EditText) findViewById(R.id.numOfTeams);
+        mCreateTeams = (Button) findViewById(R.id.createTeams);
+        mTeamOne = (ListView) findViewById(R.id.teamOne);
+        mTeamTwo = (ListView) findViewById(R.id.teamTwo);
+
         Intent intent = getIntent();
         ArrayList<String> names = intent.getStringArrayListExtra("key");
         Log.d(TAG, names.toString());
+
+        mCreateTeams.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 
 }
