ca47c3f5731873db72118a5b1861ed6da91e916b
ztwomey <zrt685@hotmail.com>
2016-03-01 22:33:37 -0500

    Changed app to pick a random winner.  Condensed app to only use the TeamMaker activity

--- app/src/main/java/com/example/zac/teammaker/ResultsActivity.java
+++ app/src/main/java/com/example/zac/teammaker/ResultsActivity.java
@@ -7,31 +7,31 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class ResultsActivity extends AppCompatActivity {
 
     public static final String TAG = ResultsActivity.class.getSimpleName();
-    public EditText mNumOfTeams;
-    public Button mCreateTeams;
-    public ListView mTeamOne;
-    public ListView mTeamTwo;
-
+    public Button mChooseWinner;
+    public ListView mContestantList;
+    public TextView mShowWinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-        mNumOfTeams = (EditText) findViewById(R.id.numOfTeams);
-        mCreateTeams = (Button) findViewById(R.id.createTeams);
-        mTeamOne = (ListView) findViewById(R.id.teamOne);
-        mTeamTwo = (ListView) findViewById(R.id.teamTwo);
+        mChooseWinner = (Button) findViewById(R.id.chooseWinner);
+        mContestantList = (ListView) findViewById(R.id.contestantList);
+        mShowWinner = (TextView) findViewById(R.id.showWinner);
 
         Intent intent = getIntent();
         final ArrayList<String> names = intent.getStringArrayListExtra("key");
@@ -39,15 +39,17 @@ public class ResultsActivity extends AppCompatActivity {
 
 
 
-        mCreateTeams.setOnClickListener(new View.OnClickListener() {
+
+        mChooseWinner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Collections.shuffle(names);
-                String number = mNumOfTeams.getText().toString();
-                int numTeams = Integer.parseInt(number);
+                Log.d(TAG, names.toString());
 
-
-
+                Random random = new Random();
+                String winner = names.get(random.nextInt(names.size()));
+                mShowWinner.setText(winner);
+                names.remove(winner);
                 Log.d(TAG, names.toString());
             }
 
--- app/src/main/java/com/example/zac/teammaker/TeamMaker.java
+++ app/src/main/java/com/example/zac/teammaker/TeamMaker.java
@@ -1,24 +1,30 @@
 package com.example.zac.teammaker;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class TeamMaker extends AppCompatActivity {
 
+    public static final String TAG = ResultsActivity.class.getSimpleName();
     public EditText mNameField;
     public Button mAddButton;
-    public Button mSubmitButton;
+    public Button mChooseWinner;
     public ListView mNameList;
+    public TextView mShowWinner;
     public ArrayList<String> mPlayerList = new ArrayList<>();
 
     @Override
@@ -28,32 +34,45 @@ public class TeamMaker extends AppCompatActivity {
 
         mNameField = (EditText) findViewById(R.id.playerNames);
         mAddButton = (Button)findViewById(R.id.addPlayer);
-        mSubmitButton = (Button)findViewById(R.id.subPlayers);
+        mChooseWinner = (Button)findViewById(R.id.chooseWinner);
         mNameList = (ListView) findViewById(R.id.nameList);
+        mShowWinner = (TextView) findViewById(R.id.showWinner);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(TeamMaker.this, android.R.layout.simple_list_item_1, mPlayerList);
 
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String getName = mNameField.getText().toString();
                 mPlayerList.add(getName);
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(TeamMaker.this, android.R.layout.simple_list_item_1, mPlayerList);
                 mNameList.setAdapter(adapter);
                 mNameField.setText("");
             }
         });
 
-        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+        mChooseWinner.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                sendResults();
+            public void onClick(View view) {
+                Collections.shuffle(mPlayerList);
+                Log.d(TAG, mPlayerList.toString());
+
+                if (mPlayerList.isEmpty()){
+                    Toast.makeText(TeamMaker.this, "Please add names to the contestant list", Toast.LENGTH_LONG).show();
+                } else{
+                    Random random = new Random();
+                    String winner = mPlayerList.get(random.nextInt(mPlayerList.size()));
+                    mShowWinner.setText(winner);
+                    mPlayerList.remove(winner);
+                    Log.d(TAG, mPlayerList.toString());
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
     }
 
-    private void sendResults() {
-        Intent intent = new Intent(this, ResultsActivity.class);
-        intent.putStringArrayListExtra("key",mPlayerList);
-        startActivity(intent);
-    }
+//    private void sendResults() {
+//        Intent intent = new Intent(this, ResultsActivity.class);
+//        intent.putStringArrayListExtra("key",mPlayerList);
+//        startActivity(intent);
+//    }
 
 }
